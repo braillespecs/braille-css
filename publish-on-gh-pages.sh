475743703101e266b2d9f9028b71bff8a5d7a56e
Bert Frees <bertfrees@gmail.com>
2022-04-29 20:00:59 +0200

    Add Github workflow to automatically publish tags

--- publish-on-gh-pages.sh
+++ publish-on-gh-pages.sh
@@ -3,22 +3,30 @@ set -x
 set -e
 CURDIR=$(cd $(dirname "$0") && pwd)
 SITE_DIR=$1
-GH_REMOTE="git@github.com:braillespecs/braille-css.git"
-BRANCH=$( git rev-parse --abbrev-ref HEAD )
-if [ -z "$( git status --porcelain )" ]; then
-	TAG=$( git tag --contains=HEAD )
-fi
-if [ -n "$TAG" ]; then
-	DEFAULT_VERSION=$TAG
+if [ -n "$GITHUB_TOKEN" ]; then
+	GH_REMOTE="https://${GITHUB_TOKEN}@github.com/braillespecs/braille-css.git"
 else
-	DEFAULT_VERSION=$BRANCH
+	GH_REMOTE="git@github.com:braillespecs/braille-css.git"
 fi
-read -p "Version? (default: $DEFAULT_VERSION) " VERSION
+BRANCH=$( git rev-parse --abbrev-ref HEAD )
+if [ -n "$( git status --porcelain )" ]; then
+	echo "Dirty working tree. Not publishing" >&2
+	exit
+fi
+TAG=$( git tag --contains=HEAD )
+if [ -z "$TAG" ]; then
+	echo "Current revision is not a tag. Not publishing" >&2
+	exit
+fi
+DEFAULT_VERSION=$TAG
+if [ -t 0 ]; then
+	read -p "Version? (default: $DEFAULT_VERSION) " VERSION
+fi
 [ -z "$VERSION" ] && VERSION=$DEFAULT_VERSION
 LATEST=$( git tag --list | tail -n1 )
 [ -z "$LATEST" ] && LATEST=master
-TMP_DIR=$( mktemp -t "$(basename "$0")" )
-rm $TMP_DIR
+TMP_DIR=$( mktemp -d "${TMPDIR:-/tmp}/$(basename "$0").XXXXXXXXX" )
+rm -r $TMP_DIR
 git clone --branch gh-pages --depth 1 $GH_REMOTE $TMP_DIR
 mkdir -p $TMP_DIR/$VERSION
 cp -r $SITE_DIR/* $TMP_DIR/$VERSION
@@ -42,6 +50,10 @@ for page in ${pages[@]}; do
 done
 cd $TMP_DIR
 git add .
+if [ -n "$GITHUB_TOKEN" ]; then
+	git config user.name "Github CI"
+	git config user.email "braille-css@users.noreply.github.com"
+fi
 git commit --amend --no-edit
 git push --force $GH_REMOTE gh-pages:gh-pages
 cd $CURDIR
